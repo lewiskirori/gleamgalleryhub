45287ac234f91f053c9b1aaa2abd9c3158338cc5
Lewis(K)Ycot <88181151+lewiskirori@users.noreply.github.com>
2025-10-30 18:32:29 +0300

    Fix type declaration

--- types/index.ts
+++ types/index.ts
@@ -4,7 +4,7 @@ export interface CustomButtonProps {
     title: string;
     containerStyles?: string;
     handleClick:
-    MouseEventHandler<HTMLButtonElement>;
+    MouseEventHandler <HTMLButtonElement>;
     btnType?: "button" | "submit";
     textStyles?: string;
     rightIcon?: string;
