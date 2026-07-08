833af2a2f73ffc51a6ab36af849a436e342dd64d
Lewis(K)Ycot <88181151+lewiskirori@users.noreply.github.com>
2026-07-08 11:28:08 +0300

    Refactor More readability

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -25,7 +25,9 @@ const ShowMore = ({ pageNumber, isNext, setLimit }: ShowMoreProps) => {
         <CustomButton 
           title="Show More"
           btnType="button"
-          containerStyles="bg-transparent border border-gray-900 text-gray-900 rounded-full min-w-[130px] transition duration-300 ease-in-out hover:bg-blue-500 hover:text-white hover:border-transparent"
+          containerStyles="bg-transparent border border-gray-900 text-gray-900 
+          rounded-full min-w-[130px] transition duration-300 ease-in-out hover:bg-blue-500 
+          hover:text-white hover:border-transparent"
           handleClick={handleNavigation}
         />
       )}
