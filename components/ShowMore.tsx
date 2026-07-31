9aaecead1b5c727ff1bbb6f1d8ffe70d48e030fd
Lewis(K)Ycot <88181151+lewiskirori@users.noreply.github.com>
2026-07-31 13:03:04 +0300

    Change limit calculation

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -9,7 +9,7 @@ import { updateSearchParams } from "@/utils";
 
 const ShowMore = ({ pageNumber, isNext, setLimit }: ShowMoreProps) => {  
   const handleNavigation = () => {
-    const newLimit = (pageNumber + 1) * 11;
+    const newLimit = (pageNumber + 1) * 10;
     
     setLimit(newLimit);
   }
